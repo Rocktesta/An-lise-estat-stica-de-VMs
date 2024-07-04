c0bb0b7988b0118896142ac08a2d9638b2e8641b
João Ebbers <122540086+Rocktesta@users.noreply.github.com>
2024-07-04 17:27:32 -0300

    Pre-alpha main.py
    Primeira versão do programa para análise gráfica, atualmente não funciona corretamente

---
+++ main.py
@@ -0,0 +1,37 @@
+import openpyxl as openxl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def readXl (xl_file):  # funcao para ler o arquivo .xl (apenas a primeira coluna da linha 2 ate 501 é lida)
+    wb = openxl.load_workbook(xl_file)
+    ws = wb.active
+    data = []
+    for row in ws.iter_rows(min_row=2, max_row=501, min_col=1, max_col=1, values_only=True):    # parametros para ler-se somente a primeira coluna com dados brutos em rol
+        data.append(row[0])
+
+    return data
+
+xl_file = "C:\\Users\\" #FIXME  
+
+def dataPlot (data_set):
+    plt.plot(data_set)
+    plt.title('Data from Excel Column A2:A500')
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.show()
+
+def frequencyTable (data):
+    n = len(data)
+    classes = np.round(np.sqrt(n))
+    max_amplitude = data[n - 1] - data[0]
+    classes_amp = int(np.ceil((max_amplitude / classes)))
+    histogram = np.histogram(data, bins=np.arange(classes_amp))
+    return print(histogram)
+
+def main ():
+    data = readXl(xl_file)
+    print(data)
+    frequencyTable(data)
+
+main()
